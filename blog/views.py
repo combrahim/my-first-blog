368558d371ae6286c479321d06ed8fd29f05f5e2
combrahim <hhamdbra@gmail.com>
2018-07-12 00:15:50 +0100

    my ass app, first commit

---
+++ blog/views.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Create your views here.
+# from django.http import HttpResponse
+# def home(request):
+	# text = """<h1>Bienvenue sur mon blog !</h1>
+	# <p>L'assebleur ça tue ce qui ne connait pas!</p>"""
+	# return HttpResponse(text)
